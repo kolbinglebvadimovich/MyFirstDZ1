f287e1f5722e2b7bfd2978f6b0deb2997fc3fe52
kolbinglebvadimovich <sonya54842@yandex.ru>
2026-02-07 12:34:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 
 int main()
-
 {
-    int veriable1{ 200 };
-    int veriable2{ 500 };
-    
-    std::cout << std::noboolalpha;
-    std::cout << veriable1 == veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 += veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 > veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 < veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 != veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 >= veriable2 << std::endl; //Решение примера.
-    std::cout << veriable1 <= veriable2 << std::endl; //Решение примера.
+    std::cout << "Hello World!!!" << std::endl;
 
     return EXIT_SUCCESS;
 
 }
+
 
 
 
